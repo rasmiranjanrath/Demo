03c297d0cf2eda315ed485c19000830b869b360a
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2020-10-27 18:21:40 +0530

    test commit

---
+++ copy.sh
@@ -0,0 +1,25 @@
+cd loyalty/target/
+cp loyalty-0.0.1-SNAPSHOT.war /home/rasmiranjan/Desktop/loyalty/
+cd ../../
+cd user-service/target/
+cp user-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/user-service/
+cd ../../
+cd loyalty-entity-service/target/
+cp loyalty-entity-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/loyalty-entity-service/
+cd ../../
+cd loyalty-management-service/target/
+cp loyalty-management-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/loyalty-management-service/
+cd ../../
+cd transaction-service/target/
+cp transaction-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/transaction-service/
+cd ../../
+cd loyalty-behavioural-service/target/
+cp loyalty-behavioural-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/behavioral-service/
+cd ../../
+cd loyalty-reports-service/target/
+cp loyalty-reports-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/loyalty-reports-service/
+cd ../../
+cd custom-offers-service/target/
+cp custom-offers-service-0.0.1-SNAPSHOT.jar /home/rasmiranjan/Desktop/loyalty-custom-offer-service/
+cd ../../
+
